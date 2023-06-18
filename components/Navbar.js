6b976cee056a0a781a4199341cbb735967144b02
Yolis <yolisisabel28@gmail.com>
2023-06-17 23:42:17 -0700

    changed pathway name from contact to project

--- components/Navbar.js
+++ components/Navbar.js
@@ -4,22 +4,24 @@ import Link from "next/link";
 
 const Navbar = () => {
   return (
-    <nav className={styles.mainnav}>
-      <ul>
-        <Link href="/">
-          <li>Home</li>
-        </Link>
-        <Link href="/about">
-          <li>About</li>
-        </Link>
-        <Link href="/blog">
-          <li>Blog</li>
-        </Link>
-        <Link href="/contact">
-          <li>Contact</li>
-        </Link>
-      </ul>
-    </nav>
+    <div className={styles.mainnav}>
+      <nav>
+        <ul>
+          <Link href="/">
+            <li>Home</li>
+          </Link>
+          <Link href="/about">
+            <li>About</li>
+          </Link>
+          <Link href="/blog">
+            <li>Blog</li>
+          </Link>
+          <Link href="/project">
+            <li>Project</li>
+          </Link>
+        </ul>
+      </nav>
+    </div>
   );
 };
 

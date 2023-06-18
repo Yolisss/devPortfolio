aebab86dacf78c3a90f76fd8b9483b9c7ee41aca
Yolis <yolisisabel28@gmail.com>
2023-06-18 00:25:40 -0700

    changed context of home page, added greeting

--- pages/index.js
+++ pages/index.js
@@ -3,5 +3,12 @@ import styles from "../styles/Home.module.css";
 import Link from "next/link";
 
 export default function Home() {
-  return <div>Welcome to my page!</div>;
+  return (
+    <div>
+      <h2 className={styles.hometext}>Hello, I'm Yolis!</h2>
+      <h4 className={styles.hometext}>
+        Junior Fullstack Developer | First Generation Latina | Boba addict
+      </h4>
+    </div>
+  );
 }

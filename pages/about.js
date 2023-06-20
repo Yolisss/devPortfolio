ab1316cb96dd6ce183809dc8a3eefc9f0cab2298
Yolis <yolisisabel28@gmail.com>
2023-06-19 23:53:30 -0700

    added about me paragraph

--- pages/about.js
+++ pages/about.js
@@ -1,5 +1,21 @@
 import React from "react";
 
 export default function About() {
-  return <div>This is my about page</div>;
+  return (
+    <div>
+      <h3>Welcome to my 'About me' page!</h3>
+      <p>
+        I am thrilled to have you here as I embark on my journey as a
+        first-generation Latina in the tech field. My goal here is not just talk
+        about my personal aspirations and achievements; it is about finding ways
+        of breaking barriers and paving the way for underrepresented individuals
+        of color who dream of entering the world of technology. I believe that
+        by sharing my experiences, challenges, and triumphs, I can inspire
+        others to take the courageous steps necessary to create a better life
+        for themselves and their families while making a lasting impact on the
+        tech industry. Join me as we shatter stereotypes, challenge norms, and
+        build a more inclusive and diverse tech community together.
+      </p>
+    </div>
+  );
 }

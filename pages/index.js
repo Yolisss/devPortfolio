d3dc00ef0bc1888218f5b6fdf2cf287f1cba17f3
Yolis <yolisisabel28@gmail.com>
2023-06-20 16:30:18 -0700

    imported image component

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import Head from "next/head";
 import styles from "../styles/Home.module.css";
 import Link from "next/link";
 import SocialMedia from "../components/SocialMedia";
+import Image from "next/image";
 
 export default function Home() {
   return (
@@ -11,7 +12,6 @@ export default function Home() {
         <h4 className={styles.hometext}>
           Junior Fullstack Developer | First Generation Latina | Boba addict
         </h4>
-        <SocialMedia />
       </div>
     </div>
   );

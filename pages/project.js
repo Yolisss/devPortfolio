1504b12ebef243f9c5483bf2616219313996a144
Yolis <yolisisabel28@gmail.com>
2023-06-19 23:54:06 -0700

    created intro paragraph and included links to jscafe and lendapaw

--- pages/project.js
+++ pages/project.js
@@ -1,5 +1,47 @@
 import React from "react";
 
 export default function Project() {
-  return <div>This is my project</div>;
+  return (
+    <div>
+      {" "}
+      <p>
+        As a junior software engineer, I am steadily building my experience in
+        the tech industry. What truly drives me is the opportunity to work on
+        applications that have a meaningful impact on others. Recently, I had
+        the privilege of creating two full stack apps that revolve around topics
+        close to my heart. One of them is "JavaSweet Cafe," a delightful coffee
+        app that brings together java lovers to explore and savor unique brews.
+        The other is "Lend A Paw," an adoption application that connects shelter
+        animals with loving homes. I take immense pride in these projects, as
+        they represent my dedication to merging my technical skills with my
+        passions. With every line of code, I strive to improve and excel, and I
+        am eager to continue honing my abilities to contribute even more to the
+        tech world.
+      </p>
+      <div>
+        <h4>JavaSweet Cafe</h4>
+        <p>
+          About JavaSweet Cafe is a PERN stack application designed to allow
+          coffee lovers to create drinks in the comfort of their own home. This
+          space allows clients to document any drinks that they may create on
+          their own.
+        </p>
+        <br />
+      </div>
+      <a href="https://github.com/Yolisss/2.0JSCafeProject" target="_blank">
+        View Project on GitHub
+      </a>
+      <br />
+      <div>
+        <h4>Lend A Paw</h4>
+        <p>
+          About Lend A Paw is an application that supports animals through
+          adoption, volunteering, donating, fostering, and more!
+        </p>
+        <a href="https://github.com/Yolisss/Lend-A-Paw" target="_blank">
+          View Project on GitHub
+        </a>
+      </div>
+    </div>
+  );
 }

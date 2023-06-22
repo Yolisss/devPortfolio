1d091043531e55e7e26d5bdfc2d83ae42c3c1a46
Yolis <yolisisabel28@gmail.com>
2023-06-22 00:02:39 -0700

    added link to resume for users

--- pages/about.js
+++ pages/about.js
@@ -1,21 +1,32 @@
 import React from "react";
+import Link from "next/link";
 
 export default function About() {
   return (
     <div>
-      <h3>Welcome to my 'About me' page!</h3>
-      <p>
-        I am thrilled to have you here as I embark on my journey as a
-        first-generation Latina in the tech field. My goal here is not just talk
-        about my personal aspirations and achievements; it is about finding ways
-        of breaking barriers and paving the way for underrepresented individuals
-        of color who dream of entering the world of technology. I believe that
-        by sharing my experiences, challenges, and triumphs, I can inspire
-        others to take the courageous steps necessary to create a better life
-        for themselves and their families while making a lasting impact on the
-        tech industry. Join me as we shatter stereotypes, challenge norms, and
-        build a more inclusive and diverse tech community together.
-      </p>
+      <div>
+        <p>
+          Welcome! I'm thrilled to have you join me on my remarkable journey
+          from customer service to the thriving world of technology. As a
+          first-generation Latina in the tech field, my goal is not just to talk
+          about my personal aspirations and achievements but also to break
+          barriers and pave the way for underrepresented individuals of color
+          who dream of entering the world of technology. Along this journey, my
+          ultimate aim is to secure a position as a software engineer, where I
+          can continue to build my knowledge and skills in the field. If you're
+          interested in learning more about my qualifications, certifications,
+          and work history, I invite you to take a look at my resume, which is
+          available for review. It showcases my relevant experiences, education,
+          and skills that make me a strong candidate for software engineering
+          roles. Feel free to reach out to me if you have any questions or would
+          like to discuss potential opportunities for collaboration. Together,
+          let's shatter stereotypes, challenge norms, and build a more inclusive
+          and diverse tech community!
+        </p>
+      </div>
+      <div>
+        <Link href="/resume.pdf">Download Resume</Link>
+      </div>
     </div>
   );
 }

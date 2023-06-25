938ec4657924ac597edd5174f0e46b04601b4a92
Yolis <yolisisabel28@gmail.com>
2023-06-24 21:56:44 -0700

    changed font style to playfair display

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,21 @@
 import Navbar from "../components/Navbar";
 import "../styles/globals.css";
 import SocialMedia from "../components/SocialMedia";
+import { Playfair_Display } from "next/font/google";
+
+const playfair_Display = Playfair_Display({
+  weight: "400",
+  subsets: ["latin"],
+});
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
-      <Navbar />
-      <Component {...pageProps} />
-      <SocialMedia />
+      <main className={playfair_Display.className}>
+        <Navbar />
+        <Component {...pageProps} />
+        <SocialMedia />
+      </main>
     </>
   );
 }

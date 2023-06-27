5383541a6e7a99298096e0f5d0c8194545e07bf1
Yolis <yolisisabel28@gmail.com>
2023-06-26 22:49:33 -0700

    added image component for logo

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,11 +1,15 @@
 import React from "react";
 import styles from "../styles/Home.module.css";
 import Link from "next/link";
+import Image from "next/image";
 
 const Navbar = () => {
   return (
     <div>
-      <nav>
+      <nav className={styles.generalnav}>
+        <div>
+          <Image src="/Yolis.png" width={200} height={200} />
+        </div>
         <ul className={styles.mainnav}>
           <li>
             <Link href="/">Home</Link>
